1aadd36e78fedcf8c238b4a49c6b275125e08c7b
cherrblyria <cherrblyria@gmail.com>
2026-06-16 07:39:26 +0700

    feat: vol/mic mute osd (not tested)

--- .config/hypr/configs/keybinds.lua
+++ .config/hypr/configs/keybinds.lua
@@ -181,15 +181,17 @@ bind("XF86AudioPrev", cmd("playerctl previous"), { locked = true })
 
 bind("XF86AudioRaiseVolume",
   cmd(
-    "volume_int=$(wpctl get-volume @DEFAULT_SINK@ | awk '{printf \"%d\", $2 * 100}') && [ \"$volume_int\" -lt 100 ] && wpctl set-volume @DEFAULT_SINK@ 5%+ && volume=$(wpctl get-volume @DEFAULT_SINK@ | awk '{printf \"%d%%\\n\", $2 * 100}') && volume_int=$(wpctl get-volume @DEFAULT_SINK@ | awk '{printf \"%d\\n\", $2 * 100}') && $HOME/.config/hypr/scripts/osd.sh Volume \"$volume\" int \"$volume_int\""),
+    "volume_int=$(wpctl get-volume @DEFAULT_SINK@ | awk '{printf \"%d\", $2 * 100}') && [ \"$volume_int\" -lt 100 ] && wpctl set-volume @DEFAULT_SINK@ 5%+ && vol=$(wpctl get-volume @DEFAULT_SINK@) && volume_pct=$(echo \"$vol\" | awk '{printf \"%d%%\", $2*100}') && volume_int=$(echo \"$vol\" | awk '{printf \"%d\", $2*100}') && (echo \"$vol\" | grep -q MUTED && $HOME/.config/hypr/scripts/osd.sh Volume \"Muted\" || $HOME/.config/hypr/scripts/osd.sh Volume \"$volume_pct\" int \"$volume_int\")"),
   { locked = true, repeating = true })
 bind("XF86AudioLowerVolume",
   cmd(
-    "wpctl set-volume @DEFAULT_SINK@ 5%- && volume=$(wpctl get-volume @DEFAULT_SINK@ | awk '{printf \"%d%%\\n\", $2 * 100}') && volume_int=$(wpctl get-volume @DEFAULT_SINK@ | awk '{printf \"%d\\n\", $2 * 100}') && $HOME/.config/hypr/scripts/osd.sh Volume \"$volume\" int \"$volume_int\""),
+    "wpctl set-volume @DEFAULT_SINK@ 5%- && vol=$(wpctl get-volume @DEFAULT_SINK@) && volume_pct=$(echo \"$vol\" | awk '{printf \"%d%%\", $2*100}') && volume_int=$(echo \"$vol\" | awk '{printf \"%d\", $2*100}') && (echo \"$vol\" | grep -q MUTED && $HOME/.config/hypr/scripts/osd.sh Volume \"Muted\" || $HOME/.config/hypr/scripts/osd.sh Volume \"$volume_pct\" int \"$volume_int\")"),
   { locked = true, repeating = true })
-bind("XF86AudioMute", cmd("wpctl set-mute @DEFAULT_SINK@ toggle"),
+bind("XF86AudioMute",
+  cmd("wpctl set-mute @DEFAULT_SINK@ toggle && status=$(wpctl get-volume @DEFAULT_SINK@ | grep -q MUTED && echo Muted || echo Unmuted) && $HOME/.config/hypr/scripts/osd.sh Volume \"$status\""),
   { locked = true, repeating = true })
-bind("XF86AudioMicMute", cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
+bind("XF86AudioMicMute",
+  cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle && status=$(wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -q MUTED && echo Muted || echo Unmuted) && $HOME/.config/hypr/scripts/osd.sh Microphone \"$status\""),
   { locked = true, repeating = true })
 bind("XF86MonBrightnessUp",
   cmd(
