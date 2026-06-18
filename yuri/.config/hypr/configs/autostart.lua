4f3724d0f23b573e1449313c04bff3b2497578ca
cherrblyria <cherrblyria@gmail.com>
2026-06-18 20:22:13 +0700

    chore(autostart): remove vicinae

--- yuri/.config/hypr/configs/autostart.lua
+++ yuri/.config/hypr/configs/autostart.lua
@@ -1,7 +1,6 @@
 hl.on("hyprland.start", function()
   hl.exec_cmd("uwsm app -- awww-daemon")
   hl.exec_cmd("systemctl --user start hyprpolkitagent")
-  hl.exec_cmd("systemctl --user start vicinae")
 
   hl.exec_cmd("wl-paste --type text --watch cliphist store")
   hl.exec_cmd("wl-paste --type image --watch cliphist store")
