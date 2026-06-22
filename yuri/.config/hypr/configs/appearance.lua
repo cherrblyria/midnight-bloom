14bb2da91f4f3ee949bd4771d028b51c963b2516
cherrblyria <cherrblyria@gmail.com>
2026-06-22 21:42:20 +0700

    style(hyprland, bar, mako): adjust gap - no windows gap - no waybar gap - more notification gap

--- yuri/.config/hypr/configs/appearance.lua
+++ yuri/.config/hypr/configs/appearance.lua
@@ -2,8 +2,8 @@ local colors = require('themes.catppuccin-mocha')
 
 hl.config({
   general = {
-    gaps_in          = 3,
-    gaps_out         = 6,
+    gaps_in          = 0,
+    gaps_out         = 0,
     border_size      = 1,
 
     col              = {
