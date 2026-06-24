cd5ca0a6ff4e7f2f5cd7037ffe86faa42922be51
cherrblyria <cherrblyria@gmail.com>
2026-06-24 21:20:41 +0700

    feat(rofi): calculator

--- yuri/.config/hypr/configs/keybinds.lua
+++ yuri/.config/hypr/configs/keybinds.lua
@@ -19,9 +19,9 @@ local bind = hl.bind
 local dsp = hl.dsp
 local dispatch = hl.dispatch
 local cmd = dsp.exec_cmd
-local rcmd = dsp.exec_raw
+-- local rcmd = dsp.exec_raw
 
-local float = { float = true, size = { "(monitor_w*0.75)", "(monitor_h*0.7)" } }
+-- local float = { float = true, size = { "(monitor_w*0.75)", "(monitor_h*0.7)" } }
 
 local MAX_ZOOM = 9
 local MIN_ZOOM = 1
@@ -55,8 +55,8 @@ bind("SUPER + Space", cmd("mb-rofi"))
 bind("SUPER + Return", cmd("footclient"))
 bind("SUPER + SHIFT + Return", cmd("footclient --title quickterminal"))
 bind("SUPER + E", cmd("footclient yazi"))
+bind("SUPER + CTRL + P", cmd("hyprpicker -aln"))
 bind("SUPER + SHIFT + E", cmd("thunar"))
-bind("SUPER + SHIFT + C", cmd("hyprpicker -aln"))
 bind("SUPER + SHIFT + A", cmd("footclient --title ft pulsemixer"))
 bind("SUPER + SHIFT + B", cmd("footclient --title ft bluetui"))
 bind("CTRL + SHIFT + Escape", cmd("footclient --title ft btop"))
@@ -64,12 +64,11 @@ bind("CTRL + SHIFT + Escape", cmd("footclient --title ft btop"))
 bind("SUPER + V", cmd("mb-rofi clipboard"))
 bind("SUPER + Period", cmd("mb-rofi emoji"))
 bind("SUPER + W", cmd("mb-wallpaper"))
+bind("SUPER + SHIFT + C", cmd("mb-rofi calc"))
 bind("SUPER + ALT + W", cmd("mb-wallpaper random"))
 bind(
   "SUPER + SHIFT + W",
-  cmd(
-    'wall_path=$(readlink -f "$HOME/.cache/wallpaper") && notify-send -h string:x-canonical-private-synchronous:wallpaper \'Wallpaper\' "$wall_path"'
-  )
+  cmd('wall_path=$(readlink -f "$HOME/.cache/wallpaper") && notify-send -h string:x-canonical-private-synchronous:wallpaper \'Wallpaper\' "$wall_path"')
 )
 
 ---- SYSTEM ----
@@ -84,16 +83,16 @@ bind("SUPER + I", function()
   dispatch(
     cmd(
       "notify-send 'Window' 'title: "
-      .. title
-      .. "\nclass: "
-      .. class
-      .. "\ninitTitle: "
-      .. initTitle
-      .. "\ninitClass: "
-      .. initClass
-      .. "\nisXwayland: "
-      .. tostring(isXwayland)
-      .. "'"
+        .. title
+        .. "\nclass: "
+        .. class
+        .. "\ninitTitle: "
+        .. initTitle
+        .. "\ninitClass: "
+        .. initClass
+        .. "\nisXwayland: "
+        .. tostring(isXwayland)
+        .. "'"
     )
   )
   dispatch(cmd("wl-copy '" .. title .. "'"))
@@ -104,11 +103,7 @@ bind("SUPER + N", cmd("notify-send 'Notification' 'Hello, World!'"))
 bind("SUPER + SHIFT + R", cmd("mb-reload"), { locked = true })
 
 bind("ALT + Space", cmd("mb-kblayout"), { locked = true })
-bind(
-  "SUPER + SHIFT + P",
-  cmd("sleep 0.2 && hyprctl dispatch 'hl.dsp.dpms({ action = \"disable\" })'"),
-  { locked = true }
-)
+bind("SUPER + SHIFT + P", cmd("sleep 0.2 && hyprctl dispatch 'hl.dsp.dpms({ action = \"disable\" })'"), { locked = true })
 bind("SUPER + ALT + L", cmd("loginctl lock-session"))
 bind("CTRL + ALT + Delete", cmd("wlogout -m 0"))
 
